8c0bb152ecbbc287f81b06cb828a04474f9ce01b
XxvalentesxX <vaban369pedro369@example.com>
2024-08-25 18:40:34 -0300

    modified:   .gitignore 	modified:   index.js

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const sendMessage = require("./functions/functions/sendMessage");
 const alwaysReply = require('./functions/events/alwaysReply');
 const banUser = require("./functions/functions/ban");
 const { jsonClear, jsonExists, jsonSet, jsonParse, jsonPretty, jsonStringify, jsonUnset, json } = require("./functions/functions/jsonFunctions");
-const loadEvents = require('./functions/functions/loadEvents')
-const checkContains = require('./function/functions/checkContains')
-const checkUserPerms = require('./functions/functions/checkPermissions')
-const { guildID, getMessageContent, stop }= require('./functions/functions/functions')
-const loop = require('./functions/functions/loop')
+const loadEvents = require('./functions/functions/loadEvents');
+const checkContains = require('./functions/functions/checkContains');
+const checkUserPerms = require('./functions/functions/checkPermissions');
+const { guildID, getMessageContent, stop }= require('./functions/functions/functions');
+const loop = require('./functions/functions/loop');
 const newCommand = require('./functions/functions/newCommands');
 const { createRole, findRole, mentionedRoles } = require("./functions/functions/role");
 const loadSlashCommandsHandler = require("./functions/functions/slashs/loadSlashCommandsHandler");
 const { subCommand, newSlashCommand } = require("./functions/functions/slashs/slashCommands");
-
+const embedCreate = require('./functions/functions/embedCreate')
 
 module.exports = {
     startBot,
