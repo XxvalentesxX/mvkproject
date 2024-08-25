ef5883b893fd272ce1d3bc59a13ab7c196cf4a32
XxvalentesxX <vaban369pedro369@example.com>
2024-08-25 18:18:51 -0300

    modified:   .gitignore 	deleted:    functions/functions/users.js 	modified:   index.js

--- index.js
+++ index.js
@@ -1,6 +1,58 @@
-const { startBot } = require("./functions/functions/startBot");
-const loadHandler = require('./functions/functions/loadHandler')
+const startBot = require("./functions/functions/startBot");
+const loadHandler = require('./functions/functions/loadHandler');
+const sendMessage = require("./functions/functions/sendMessage");
+const alwaysReply = require('./functions/events/alwaysReply');
+const { setServerVar, setUserVar, setChannelVar, setVar, getVar, getChannelVar, getUserVar, getServerVar, variableCreate, varExists } = require('./functions/functions/variables');
+const banUser = require("./functions/functions/ban");
+const { jsonClear, jsonExists, jsonSet, jsonParse, jsonPretty, jsonStringify, jsonUnset, json } = require("./functions/functions/jsonFunctions");
+const loadEvents = require('./functions/functions/loadEvents')
+const checkContains = require('./function/functions/checkContains')
+const checkUserPerms = require('./functions/functions/checkPermissions')
+const { guildID, getMessageContent, stop }= require('./functions/functions/functions')
+const loop = require('./functions/functions/loop')
+const newCommand = require('./functions/functions/newCommands');
+const { createRole, findRole, mentionedRoles } = require("./functions/functions/role");
+const loadSlashCommandsHandler = require("./functions/functions/slashs/loadSlashCommandsHandler");
+const { subCommand, newSlashCommand } = require("./functions/functions/slashs/slashCommands");
+
+
 module.exports = {
     startBot,
-    loadHandler
+    loadHandler,
+    sendMessage,
+    embedCreate,
+    alwaysReply,
+    setServerVar,
+    setUserVar,
+    setChannelVar,
+    setVar,
+    getVar,
+    getChannelVar,
+    getUserVar,
+    getServerVar,
+    variableCreate,
+    varExists,
+    banUser,
+    jsonClear,
+    jsonExists,
+    jsonSet,
+    jsonParse,
+    jsonPretty,
+    jsonStringify,
+    jsonUnset,
+    json,
+    loadEvents,
+    checkContains,
+    checkUserPerms,
+    guildID,
+    getMessageContent,
+    stop,
+    loop,
+    createRole,
+    findRole,
+    mentionedRoles,
+    newCommand,
+    loadSlashCommandsHandler,
+    subCommand,
+    newSlashCommand
 }
