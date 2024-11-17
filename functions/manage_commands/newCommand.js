67c58c640ae89e8e2970e1c96a416e75cb794f59
XxvalentesxX <vaban369pedro369@example.com>
2024-11-17 14:40:37 -0300

    modified:   functions/manage_commands/newCommand.js

--- functions/manage_commands/newCommand.js
+++ functions/manage_commands/newCommand.js
@@ -7,35 +7,24 @@ function newCommand({ name, aliases = [], code }) {
     return;
   }
 
-<<<<<<< HEAD:functions/manage_commands/newCommand.js
-  // Almacenar el comando principal
   commands.set(name, { code });
 
-  // Almacenar los alias si los hay
   aliases.forEach(alias => {
     commands.set(alias, { code });
-=======
-  commands.set(name.toLowerCase(), { description, code });
-
-  aliases.forEach(alias => {
-    commands.set(alias.toLowerCase(), { description, code });
->>>>>>> f7f962b47f36e5399534fa7ae81f8ac34dc605d8:functions/manage_commands/newCommands.js
   });
 }
 
 async function handleCommand(message) {
-  const prefix = message.client.prefix.toLowerCase();
-  if (!message.content.toLowerCase().startsWith(prefix)) return;
+  if (!message.content.startsWith(message.client.prefix)) return;
 
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const args = message.content.slice(message.client.prefix.length).trim().split(/ +/);
   const commandName = args.shift().toLowerCase();
 
-  // Buscar el comando usando el nombre o alias
   const command = commands.get(commandName);
 
   if (command) {
     try {
-      await command.code(message, args); // Ejecutar el código del comando
+      await command.code(message, args);
     } catch (error) {
       console.error(`Error executing command ${commandName}:`, error);
     }
