efdfde13ac8d568f382d8dbfacf434627f829a9e
XxvalentesxX <vaban369pedro369@example.com>
2024-11-30 19:07:38 -0300

    modified:   functions/miscelaneos/variables.js

--- functions/miscelaneos/variables.js
+++ functions/miscelaneos/variables.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const BASE_DIR = path.join('./', 'variables');
+const BASE_DIR = path.join('..', '..', '..', '..', 'variables');
 
 class Var {
     static Type = {
