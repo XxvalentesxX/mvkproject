8699cc29ae7279756a44a7cce7fe74765690e88e
XxvalentesxX <vaban369pedro369@example.com>
2024-11-30 19:05:46 -0300

    modified:   index.js

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const loadCommands = require('./functions/handlers/loadCommands');
 const { sendMessage } = require("./functions/miscelaneos/sendMessage");
 const banUser = require("./functions/moderation/banUser");
 const { jsonClear, jsonExists, jsonSet, jsonParse, jsonPretty, jsonStringify, jsonUnset, json } = require("./functions/utils/jsonFunctions");
-const loadEvents = require('./functions/handlers/loadEvents');
 const checkContains = require('./functions/utils/checkContains');
 const { checkUserPerms } = require('./functions/utils/checkPermissions');
 const { getMessageContent, stop }= require('./functions/utils/stopAndMessage');
 const loop = require('./functions/utils/loop');
 const { newCommand } = require('./functions/manage_commands/newCommand');
-const loadSlashs = require("./functions/handlers/loadSlashs");
 const { subCommand, newSlashCommand, slashOption } = require("./functions/manage_commands/slashCommands");
 const embedCreate = require('./functions/utils/embedCreate');
 const timeoutUser = require("./functions/moderation/timeout");
-const { addButtonInteraction, createButton } = require("./functions/components/buttons");
 const { splitText, textSplit, getTextSplitLength } = require("./functions/utils/splits");
 const { newEvent } = require("./functions/manage_commands/newEvents");
 const { createChannel } = require("./functions/administration/createChannel");
@@ -24,9 +20,10 @@ const addReactions = require('./functions/miscelaneos/addReactions');
 const unbanUser = require('./functions/moderation/unbanUser');
 const setPresence = require('./functions/setPresence');
 const Var = require('./functions/miscelaneos/variables');
+const Component = require('./functions/manage_commands/Components');
+const Load = require('./functions/handlers/Load');
 
 module.exports = {
-    loadCommands,
     sendMessage,
     embedCreate,
     banUser,
@@ -38,19 +35,15 @@ module.exports = {
     jsonStringify,
     jsonUnset,
     json,
-    loadEvents,
     checkContains,
     checkUserPerms,
     getMessageContent,
     stop,
     loop,
-    loadSlashs,
     subCommand,
     newSlashCommand,
     newCommand,
     timeoutUser,
-    createButton,
-    addButtonInteraction,
     splitText,
     textSplit,
     getTextSplitLength,
@@ -64,5 +57,7 @@ module.exports = {
     addReactions,
     unbanUser,
     setPresence,
-    Var
+    Var,
+    Component,
+    Load
 };
