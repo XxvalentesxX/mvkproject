7f74c19db550988afb8fc01a3b716725d1ffada3
XxvalentesxX <vaban369pedro369@example.com>
2024-12-01 15:32:54 -0300

    modified:   functions/miscelaneos/sendMessage.js

--- functions/miscelaneos/sendMessage.js
+++ functions/miscelaneos/sendMessage.js
@@ -1,11 +1,16 @@
-async function sendMessage({ content, embeds = [], ephemeral = false, channel, components = [], replyTo = null }) {
+async function sendMessage({ content, embeds = [], ephemeral = false, channel, components = [], replyTo = null, interaction = null }) {
   try {
     const messageOptions = {
       content,
       embeds,
       components,
-      ephemeral
     };
+
+    if (interaction) {
+      if (ephemeral) messageOptions.ephemeral = true;
+      const replyMessage = await interaction.reply({ ...messageOptions, fetchReply: !ephemeral });
+      return replyMessage.id;
+    }
 
     if (replyTo) {
       const replyMessage = await replyTo.reply(messageOptions);
