72b4d08a8e2a33087874c5a0a8a9dbd72a89ef9f
XxvalentesxX <vaban369pedro369@example.com>
2024-12-13 03:05:40 -0300

    modified:   functions/classes.js

--- functions/classes.js
+++ functions/classes.js
@@ -2,16 +2,35 @@ const { createChannel } = require("./admin/channels/Create");
 const removeRole = require("./admin/roles/Remove");
 const addRole = require("./admin/roles/Add");
 const addReactions = require("./message/Reactions");
+const setPresence = require("./client/setPresence");
+const { startBot } = require("./client/startBot");
+const changeNick = require("./mod/changeNick");
+const banUser = require("./mod/banUser");
+const unbanUser = require("./mod/unbanUser");
+const timeoutUser = require("./mod/timeout");
+const loop = require("./misc/loop");
 
 module.exports = {
     Add: {
         Role: addRole,
-        Reactions: addReactions
+        Message: {
+            Reactions: addReactions
+        }
     },
     Create: {
         Channel: createChannel
     },
     Remove: {
         Role: removeRole
-    }
+    },
+    Set: {
+        Presence: setPresence
+    },
+    User: {
+        Ban: banUser,
+        Unban: unbanUser,
+        Timeout: timeoutUser,
+        SetNick: changeNick
+    },
+    Loop: loop
 }
