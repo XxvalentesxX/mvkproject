49aaddd0894261471ab701ac072462f1808e6032
XxvalentesxX <vaban369pedro369@example.com>
2024-12-13 03:05:26 -0300

    modified:   index.js

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 const { sendMessage } = require("./functions/message/Message");
-const banUser = require("./functions/mod/banUser");
 const checkContains = require('./functions/message/checkContains');
 const { checkUserPerms } = require('./functions/permissions/checkPermissions');
-const loop = require('./functions/misc/loop');
-const { newCommand } = require('./functions/handlers/add-ons/SetCommand');
 const { subCommand, newSlashCommand, slashOption } = require("./functions/handlers/add-ons/SetSlashCommand");
 const embedCreate = require('./functions/message/Embed');
-const timeoutUser = require("./functions/mod/timeout");
 const { splitText, textSplit, getTextSplitLength } = require("./functions/message/Split");
-const { newEvent } = require("./functions/handlers/add-ons/SetEvent");
-const { createChannel } = require("./functions/admin/channels/Create");
 const { startBot } = require('./functions/client/startBot');
-const addRole = require('./functions/admin/roles/Add');
-const removeRole = require('./functions/admin/roles/Remove');
-const changeNick = require('./functions/mod/changeNick');
-const addReactions = require('./functions/message/Reactions');
-const unbanUser = require('./functions/mod/unbanUser');
-const setPresence = require('./functions/client/setPresence');
 
+const { Set, Add, Create, Remove, User, Loop } = require('./functions/classes')
 const Var = require('./functions/misc/variables');
 const Load = require('./functions/handlers/loader/Load');
 const { Buttons } = require("./functions/message/Components/Buttons");
@@ -26,27 +15,21 @@ const { Menus } = require("./functions/message/Components/Menus");
 module.exports = {
     sendMessage,
     embedCreate,
-    banUser,
     checkContains,
     checkUserPerms,
-    loop,
     subCommand,
     newSlashCommand,
-    newCommand,
-    timeoutUser,
     splitText,
     textSplit,
     getTextSplitLength,
     slashOption,
-    newEvent,
-    createChannel,
     startBot,
-    addRole,
-    removeRole,
-    changeNick,
-    addReactions,
-    unbanUser,
-    setPresence,
+    Set,
+    Add,
+    Create,
+    Remove,
+    User,
+    Loop,
     Var,
     Load,
     Buttons,
