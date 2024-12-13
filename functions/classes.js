02c38a6793303dfcfb61717a545a1973d57e170a
XxvalentesxX <vaban369pedro369@example.com>
2024-12-13 02:42:05 -0300

    modified:   functions/classes.js

--- functions/classes.js
+++ functions/classes.js
@@ -1,7 +1,17 @@
-const addRole = require("./admin/Role");
+const { createChannel } = require("./admin/channels/Create");
+const removeRole = require("./admin/roles/Remove");
+const addRole = require("./admin/roles/Add");
+const addReactions = require("./message/Reactions");
 
 module.exports = {
     Add: {
-        Role: addRole
+        Role: addRole,
+        Reactions: addReactions
+    },
+    Create: {
+        Channel: createChannel
+    },
+    Remove: {
+        Role: removeRole
     }
 }
