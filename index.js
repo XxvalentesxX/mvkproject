167ab9887005177db7c4045d6eeea8c8e2bf4454
XxvalentesxX <vaban369pedro369@example.com>
2024-12-13 21:56:20 -0300

    modified:   index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { sendMessage } = require("./functions/message/Message");
 const checkContains = require('./functions/message/checkContains');
 const { subCommand, newSlashCommand, slashOption } = require("./functions/handlers/add-ons/SetSlashCommand");
-const embedCreate = require('./functions/message/Embed');
 const { startBot } = require('./functions/client/startBot');
 
 const { Set, Add, Create, Remove, User, Loop, Message } = require('./functions/classes')
@@ -9,10 +8,10 @@ const Var = require('./functions/misc/variables');
 const Load = require('./functions/handlers/loader/Load');
 const { Buttons } = require("./functions/message/Components/Buttons");
 const { Menus } = require("./functions/message/Components/Menus");
+const Embed = require("./functions/message/Embed");
 
 module.exports = {
     sendMessage,
-    embedCreate,
     checkContains,
     subCommand,
     newSlashCommand,
@@ -28,5 +27,6 @@ module.exports = {
     Var,
     Load,
     Buttons,
-    Menus
+    Menus,
+    Embed
 };
