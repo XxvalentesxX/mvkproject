fc59945be9bb74ebdb3e1beab7cf8c3421285eae
XxvalentesxX <vaban369pedro369@example.com>
2024-12-13 21:58:12 -0300

    modified:   functions/misc/loop.js

--- functions/misc/loop.js
+++ functions/misc/loop.js
@@ -1,6 +1,6 @@
 function loop({ cantidad, codigo }) {
     if (typeof cantidad !== 'number' || cantidad <= 0) {
-        throw new Error('The quantity must be a number greater than 0.');
+        return console.error('The quantity must be a number greater than 0.');
     }
     for (let i = 0; i < cantidad; i++) {
         codigo(i);
