568cd50c6736b98c4866f4a8166a5f2dbc54a0a4
XxvalentesxX <vaban369pedro369@example.com>
2024-12-13 21:57:49 -0300

    modified:   functions/message/Reactions.js

--- functions/message/Reactions.js
+++ functions/message/Reactions.js
@@ -3,20 +3,20 @@ const { getClient } = require('../client/startBot');
 async function addReactions({ message, reactions }) {
   try {
     const client = getClient();
-    if (!client) throw new Error('Cliente no disponible');
+    if (!client) return console.error('Client not available');
 
     const targetMessage = await client.channels.cache
       .get(message.channelId)
       .messages.fetch(message.id);
 
-    if (!targetMessage) throw new Error('Mensaje no encontrado');
+    if (!targetMessage) return console.error('Message not found');
 
     for (const reaction of reactions) {
       await targetMessage.react(reaction);
     }
 
   } catch (error) {
-    console.error('Error al agregar reacciones:', error.message);
+    console.error('Error while adding reactions:', error.message);
   }
 }
 
